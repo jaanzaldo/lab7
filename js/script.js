d641fda877410e68006c0c1f78d78c3eac3fe671
Jorge Anzaldo <jaanzaldo12@gmail.com>
2021-04-12 20:41:01 -0500

    changed name of button variable, added code to change value of h2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
  
-	var button= document.getElementById('entrybutton');
+	var alertbutton = document.getElementById('entrybutton');
 	var input = document.getElementById('entryinput');
 	var output = document.getElementById('textoutput');
-	button.addEventListener('click', function(){
+	alertbutton.addEventListener('click', function(){
 	
 		var alertText = "Jorge Anzaldo: " + input.value;
 		alert(alertText);
-
+		output.innerHTML = input.value;
+		
 	});
 
 
