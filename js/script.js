29a8be66af2ca00ca608880556101828703bda8e
Jorge Anzaldo <jaanzaldo12@gmail.com>
2021-04-12 20:36:00 -0500

    Added name and email, added variables for everything requiring document.getElementById, created event listener, added alert

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 function init(){
 //add your javascrip between these two lines of code
  
+	var button= document.getElementById('entrybutton');
+	var input = document.getElementById('entryinput');
+	var output = document.getElementById('textoutput');
+	button.addEventListener('click', function(){
+	
+		var alertText = "Jorge Anzaldo: " + input.value;
+		alert(alertText);
+
+	});
 
 
 
 
 
-
-
+}
 window.addEventListener('load', init);
